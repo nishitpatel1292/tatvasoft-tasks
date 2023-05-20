ec8ebac4f23e52e9d4aa951fa881b5b79c9adcc2
nishitpatel1292 <73834549+nishitpatel1292@users.noreply.github.com>
2023-05-20 08:54:07 +0530

    created

---
+++ src/components/MyFunction.js
@@ -0,0 +1,9 @@
+export default function myFunction(props){
+    
+    console.log("Introducing tatvasoft tasks");
+    console.log("I am ",props.name,"from",props.city);
+    return(
+        <>
+        </>
+    );
+}
